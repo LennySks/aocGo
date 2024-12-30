77cb893564181da20d9b4c3dae1de622b010ba21
dxvee <david.pietrzak@student.kdg.be>
2024-12-30 19:21:41 +0100

    part 2 sketch

--- day-3/main2.go
+++ day-3/main2.go
@@ -11,21 +11,48 @@ import (
 )
 
 func main() {
+
+	var Reset = "\033[0m"
+	var Red = "\033[31m"
+	var Green = "\033[32m"
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(file)
-	r, _ := regexp.Compile(`mul\(([0-9]+),([0-9]+)\)`)
+	reMul := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	reDont := regexp.MustCompile(`don't\(\).*?do\(\)`) // Returns everything between don't() and do() in "reverse"
+
 	sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		foundMatches := r.FindAllStringSubmatch(line, -1)
-		for _, match := range foundMatches {
-			num1, _ := strconv.Atoi(match[1])
-			num2, _ := strconv.Atoi(match[2])
-			sum += num1 * num2
+		match := reDont.ReplaceAllString(line, "")
+		matches := reDont.FindAllStringIndex(line, -1)
+		foundMatches := reMul.FindAllStringSubmatch(match, -1)
+
+		//fmt.Println(foundMatches)
+
+		for _, foundMatch := range foundMatches {
+			//fmt.Println(Red + foundMatch[1] + foundMatch[2] + Reset)
+			first, _ := strconv.Atoi(foundMatch[1])
+			second, _ := strconv.Atoi(foundMatch[2])
+			sum += first * second
 		}
+
+		// Build the colored line
+		coloredLine := ""
+		lastIndex := 0
+		for _, match := range matches {
+			// Append the text before the match
+			coloredLine += Green + line[lastIndex:match[0]]
+			// Append the matched text in red
+			coloredLine += Red + line[match[0]:match[1]] + Reset
+			// Update the last index
+			lastIndex = match[1]
+		}
+		// Append the remaining text after the last match
+		coloredLine += Green + line[lastIndex:] + Reset
+		fmt.Println(coloredLine)
 	}
-	fmt.Printf("Total sum: %d", sum)
+	fmt.Println(sum)
 }
